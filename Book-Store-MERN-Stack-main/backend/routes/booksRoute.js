d6de92835b434930dbcad1db89519b3ceb054f3c
Sahil Aggarwal <102357775+wertnvgjnvj@users.noreply.github.com>
2023-11-28 20:33:30 +0530

    Update booksRoute.js

--- Book-Store-MERN-Stack-main/backend/routes/booksRoute.js
+++ Book-Store-MERN-Stack-main/backend/routes/booksRoute.js
@@ -2,8 +2,6 @@ import express from 'express';
 import { Book } from '../models/bookModel.js';
 
 const router = express.Router();
-
-// Route for Save a new Book
 router.post('/', async (request, response) => {
   try {
     if (
@@ -30,7 +28,7 @@ router.post('/', async (request, response) => {
   }
 });
 
-// Route for Get All Books from database
+
 router.get('/', async (request, response) => {
   try {
     const books = await Book.find({});
@@ -45,7 +43,7 @@ router.get('/', async (request, response) => {
   }
 });
 
-// Route for Get One Book from database by id
+
 router.get('/:id', async (request, response) => {
   try {
     const { id } = request.params;
@@ -59,7 +57,6 @@ router.get('/:id', async (request, response) => {
   }
 });
 
-// Route for Update a Book
 router.put('/:id', async (request, response) => {
   try {
     if (
